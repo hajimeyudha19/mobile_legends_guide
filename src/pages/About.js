025df5d7ebde7bfc9094746d217c36766e5f534f
Hajime Yudha <63109891+hajimeyudha19@users.noreply.github.com>
2021-11-24 19:34:16 +0700

    ACC

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="about">
+      <div className="about-app">
+        <h1>About Mobile Legends Guide</h1>
+        <p>
+          Aplikasi ini dibuat untuk memenuhi projek akhir Praktikum MDP 2021.
+        </p>
+      </div>
+      <div className="about-me">
+        <h2>About Hajime</h2>
+        <img src="/hajime.jpg" alt="foto hajime" />
+        <p>Nama: Hajime Yudha Iqbal Pranata</p>
+        <p>NIM: 21120119120017</p>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home" id="home">
+      <h1>Welcome To</h1>
+      <h1>Mobile Legends Guide</h1>
+      <h2>By Hajime Yudha I. P.</h2>
+    </div>
+  )
+}
